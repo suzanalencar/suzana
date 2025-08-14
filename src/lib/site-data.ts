de095cc9526130901d3ccab4db4fce82e0b1e091
José Vitor Alencar <alencarhub@gmail.com>
2025-08-14 10:48:09 +0000

    feat: value update

--- src/lib/site-data.ts
+++ src/lib/site-data.ts
@@ -92,7 +92,7 @@ export const siteData = {
     title: "Custos",
     goal: 98322.41,
     goalEUR: 14615,
-    current: 2665.34,
+    current: 2865.34,
     currency: "BRL",
     budget: [
       { item: "Seguro saúde", cost: 11644.50, costEUR: 1731 },
